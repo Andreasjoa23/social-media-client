ec45f0eea24bea878d17eb50e0697f54f8a9e2cb
Andreas S. Joakimsen <andjoa00220@stud.noroff.no>
2024-10-18 09:58:47 +0200

    fixed logout function

---
+++ cypress/e2e/test/logout.cy.js
@@ -0,0 +1,19 @@
+describe("Logout test", () => {
+  beforeEach(() => {
+    localStorage.setItem("token", "sample-token");
+    localStorage.setItem("profile", JSON.stringify({ name: "Test User" }));
+    cy.visit("/");
+  });
+  it("should allow user to logout with the logout button", () => {
+    cy.get('button[data-auth="logout"]').click();
+
+    cy.window().then((window) => {
+      expect(window.localStorage.getItem("token")).to.be.null;
+      expect(window.localStorage.getItem("profile")).to.be.null;
+    });
+
+    cy.url().should("eq", `${window.location.origin}/`);
+
+    cy.get('button[data-auth="logout"]').should("be.visible");
+  });
+});
